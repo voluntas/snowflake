74094a7ff2d52f9f7e8edb335c625d1814d04ebe
Ryosuke Nakai <twisted.mind@voluntas.net>
2011-04-13 01:06:53 +0900

    Add proper test in eunit test.

--- test/snowflake_mnesia_tests.erl
+++ test/snowflake_mnesia_tests.erl
@@ -2,3 +2,17 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
+snowflake_mnesia_props_test_() ->
+  {setup,
+    fun() ->
+      ok
+    end,
+    fun(_) ->
+      ok
+    end,
+    {timeout, 10000,
+      [
+        ?_assertEqual([], proper:module(prop_snowflake_mnesia))
+      ]
+    }
+  }.
