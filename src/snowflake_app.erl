c9831a564b243d6a09ef5b78d1252a15a8333af2
Ryosuke Nakai <twisted.mind@voluntas.net>
2012-02-26 00:02:45 +0900

    Add lager sample

--- src/snowflake_app.erl
+++ src/snowflake_app.erl
@@ -2,14 +2,11 @@
 
 -behaviour(application).
 
-%% Application callbacks
 -export([start/2, stop/1]).
 
-%% ===================================================================
-%% Application callbacks
-%% ===================================================================
-
 start(_StartType, _StartArgs) ->
+    lager:error("あいうえお", []),
+    lager:info("abcdefg", []),
     snowflake_sup:start_link().
 
 stop(_State) ->
