7c153e853e624b9aeefe6c0e83003b92c8ecbd32
Ryosuke Nakai <twisted.mind@voluntas.net>
2012-03-03 21:24:55 +0900

    Send serializd json message

--- src/snowflake.erl
+++ src/snowflake.erl
@@ -3,13 +3,18 @@
 -export([main/0]).
 
 main() ->
+  application:start(erlzmq),
+  application:start(jiffy),
   {ok, Context} = erlzmq:context(),
   {ok, Client} = erlzmq:socket(Context, [req]),
   ok = erlzmq:setsockopt(Client, identity, pid_to_list(self())),
   ok = erlzmq:connect(Client, "tcp://127.0.0.1:5555"),
-  erlzmq:send(Client, <<"Hello">>),
+  Request = jiffy:encode({[{foo, [<<"bing">>, 2.3, true]}]}),
+  io:format("Request(encoded): ~w", [Request]),
+  erlzmq:send(Client, Request),
   {ok, Reply} = erlzmq:recv(Client),
-  io:format("Reply: ~p", [Reply]),
+  Term = jiffy:decode(Reply),
+  io:format("Reply(decoded): ~w", [Term]),
   ok = erlzmq:close(Client),
   ok = erlzmq:term(Context),
   ok.
