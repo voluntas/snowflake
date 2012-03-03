0d15313ab5c31bfb18e5d744286fa9fe250166fc
Ryosuke Nakai <twisted.mind@voluntas.net>
2012-03-03 19:54:46 +0900

    Add REQ sample

--- src/snowflake.erl
+++ src/snowflake.erl
@@ -1,8 +1,17 @@
 -module(snowflake).
 
--export([my_func/0]).
+-export([main/0]).
 
-my_func() ->
+main() ->
+  {ok, Context} = erlzmq:context(),
+  {ok, Client} = erlzmq:socket(Context, [req]),
+  ok = erlzmq:setsockopt(Client, identity, pid_to_list(self())),
+  ok = erlzmq:connect(Client, "tcp://127.0.0.1:5555"),
+  erlzmq:send(Client, <<"Hello">>),
+  {ok, Reply} = erlzmq:recv(Client),
+  io:format("Reply: ~p", [Reply]),
+  ok = erlzmq:close(Client),
+  ok = erlzmq:term(Context),
   ok.
 
 -ifdef(TEST).
