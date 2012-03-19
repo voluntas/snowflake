b29e354f3aa9d3e15421152d8156ee787fc384a2
swdyh <youhei@gmail.com>
2012-03-19 14:22:30 +0900

    removed benchmark module.

--- bench.js
+++ bench.js
@@ -1,7 +1,4 @@
-// for node.js
-// npm install benchmark microtime
 var fs = require('fs')
-var Benchmark = require('benchmark')
 
 function bench(binary) {
     return JSON.stringify(JSON.parse(binary))
@@ -9,11 +6,8 @@ function bench(binary) {
 
 var binary = fs.readFileSync('tokoroten.json')
 var n = 100
-var s = new Benchmark.Suite()
-s.add('json', function() {
-    for (var i = 0; i < n ; i++) {
-        bench(binary)
-    }
-}).on('complete', function() {
-    console.log(this[0].times.period * 1000 * 1000)
-}).run()
+var t = new Date().getTime()
+for (var i = 0; i < n ; i++) {
+    bench(binary)
+}
+console.log((new Date().getTime() - t)  * 1000)
