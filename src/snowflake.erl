6542bcc3f8a8e46ef1a3f2da173cd3fcfc68fd4c
Ryosuke Nakai <twisted.mind@voluntas.net>
2012-12-15 14:46:51 +0900

    Add "bad" return code

--- src/snowflake.erl
+++ src/snowflake.erl
@@ -1,6 +1,20 @@
 -module(snowflake).
 
--callback start() -> ok.
+-export([main/0]).
+
+
+main() ->
+    case get_value(spam, [{eggs, <<"bacon">>}]) of
+        undefined ->
+            {error, not_found};
+        Value ->
+            {ok, Value}
+    end.
+
+
+%% -spec get_value(atom(), [{atom(), binary()}]) -> not_found | binary().
+get_value(Key, List) ->
+    proplists:get_value(Key, List, not_found).
 
 -ifdef(TEST).
 -endif.
