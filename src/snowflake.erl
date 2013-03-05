bf234c5b5096bd572f1782c27e17ac9d9b28540b
Ryosuke Nakai <twisted.mind@voluntas.net>
2013-03-05 12:15:29 +0900

    Add ejson and jsx

--- src/snowflake.erl
+++ src/snowflake.erl
@@ -3,16 +3,35 @@
 -export([main/0]).
 -export([jiffy/1]).
 -export([mochijson2/1]).
+-export([ejson/1]).
+-export([jsx/1]).
 
 -define(TIME, 100).
 
 main() ->
   {ok, Binary} = file:read_file("tokoroten.json"),
   {JiffyTime, _Value} = timer:tc(?MODULE, jiffy, [Binary]),
-  io:format("jiffy .. Time: ~w~n", [JiffyTime]),
+  {Ejson, _Value} = timer:tc(?MODULE, ejson, [Binary]),
   {Mochijson2Time, _Value} = timer:tc(?MODULE, mochijson2, [Binary]),
+  {JsxTime, _Value} = timer:tc(?MODULE, jsx, [Binary]),
+
+  io:format("jiffy      .. Time: ~w~n", [JiffyTime]),
+  io:format("ejson      .. Time: ~w~n", [Ejson]),
   io:format("mochijson2 .. Time: ~w~n", [Mochijson2Time]),
+  io:format("jsx        .. Time: ~w~n", [JsxTime]),
   ok.
+
+jsx(Binary) ->
+  F = fun(_) ->
+        jsx:decode(jsx:encode(Binary))
+      end,
+  lists:foreach(F, lists:duplicate(?TIME, duplicate)).
+
+ejson(Binary) ->
+  F = fun(_) ->
+        ejson:decode(ejson:encode(Binary))
+      end,
+  lists:foreach(F, lists:duplicate(?TIME, duplicate)).
 
 mochijson2(Binary) ->
   F = fun(_) ->
